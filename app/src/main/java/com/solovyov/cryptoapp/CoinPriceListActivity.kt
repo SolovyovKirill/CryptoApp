c889b4256bafcc598f1c8c515054ca2800336417
SolovyovKirill <Solowyow.kirya@gmail.com>
2021-12-06 22:54:18 +0300

    Creating the first layouts

--- app/src/main/java/com/solovyov/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/solovyov/cryptoapp/CoinPriceListActivity.kt
@@ -4,18 +4,17 @@ package com.solovyov.cryptoapp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.solovyov.cryptoapp.viewmodel.CoinViewModel
 
-class MainActivity : AppCompatActivity() {
+class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_coin_price_list)
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
 //        viewModel.priceList.observe(this, Observer {
 //            Log.d("TEST_OF_LOADING_DATA", "Success in Activity: $it")
